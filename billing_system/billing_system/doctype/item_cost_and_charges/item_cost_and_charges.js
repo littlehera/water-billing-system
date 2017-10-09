7be3207c30c31cc1e931598f358f29c3c1529ad2
littlehera <villanuevahera@gmail.com>
2017-10-09 11:36:35 +0800

    Added BIlling Items

---
+++ billing_system/billing_system/doctype/item_cost_and_charges/item_cost_and_charges.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, renzodemie & littehera and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Item Cost and Charges', {
+	refresh: function(frm) {
+
+	}
+});
